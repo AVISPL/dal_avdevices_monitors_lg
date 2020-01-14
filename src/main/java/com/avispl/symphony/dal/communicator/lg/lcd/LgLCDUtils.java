e9b9da076642649da866802093acba56f59bfe3a
Jonathan Gillot <jonathan.gillot@avispl.com>
2020-01-14 10:32:24 +0400

    -Initial Commit

---
+++ src/main/java/com/avispl/symphony/dal/communicator/lg/lcd/LgLCDUtils.java
@@ -0,0 +1,39 @@
+package com.avispl.symphony.dal.communicator.lg.lcd;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LgLCDUtils {
+
+    static byte[] buildSendString(byte monitorID, byte[] command, byte[] param) {
+        List<Byte> bytes = new ArrayList<>();
+
+        for (int i = 0; i < command.length; i++) {
+            bytes.add(command[i]);
+        }
+        bytes.add((byte) ' ');
+        if(monitorID < 17){
+            bytes.add((byte) '0');
+        }
+
+        String ID = Integer.toHexString(monitorID);
+
+        for(byte b:ID.getBytes()){
+            bytes.add(b);
+        }
+
+        bytes.add((byte) ' ');
+        for (int i = 0; i < param.length; i++) {
+            bytes.add(param[i]);
+        }
+        bytes.add((byte) '\r');
+
+        byte[] byteArray = new byte[bytes.size()];
+        for (int i = 0; i < bytes.size(); i++) {
+            byteArray[i] = bytes.get(i);
+        }
+
+        return byteArray;
+    }
+}
